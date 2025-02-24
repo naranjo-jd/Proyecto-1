26a2b61f5fe60fe53f70c83ea1812c810702a7c0
juanaranjo <juand8@javerianacali.edu.co>
2025-02-24 16:14:32 -0500

    metricas e inercia. test con inercia. inercia 2d corresponde a la real, inercia 3d no

--- kmeans/metric.py
+++ kmeans/metric.py
@@ -1,2 +1,17 @@
 import numpy as np
 
+def euclidean(x,y):
+    return np.linalg.norm(np.array(x) - np.array(y))
+
+def manhattan(x,y):
+    return sum(abs(a - b) for a, b in zip(x, y))
+
+def chebyshev(x,y):
+    return max(abs(a - b) for a, b in zip(x, y))
+
+def inertia(clusters, centroids, metric, q):
+    inert = 0
+    for i, cluster in enumerate(clusters):
+        for point in cluster:
+            inert += metric(point, centroids[i])**q
+    return inert
--- kmeans/tests.py
+++ kmeans/tests.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import clustering as clust
 import graphers as grph
+import metric
 
 data_2d = pd.read_csv("data_2d.csv")
 data_3d = pd.read_csv("data_3d.csv")
@@ -11,8 +12,12 @@ centroides = clust.kmeans(data_2d, 2, 5)
 grph.plot_2d(data_2d, centros)
 grph.plot_2d(data_2d, centroides)
 
+print("Inercia 2d:", metric.inertia(clust.cluster(data_2d, 2, centroides), centroides, metric.euclidean, 2))
+
 # Test 3d
 centros = clust.centroids(data_3d, 3, 5)
 centroides = clust.kmeans(data_3d, 3, 5)
 grph.plot_3d(data_3d, centros)
 grph.plot_3d(data_3d, centroides)
+
+print("Inercia 3d:", metric.inertia(clust.cluster(data_3d, 3, centroides), centroides, metric.euclidean, 2))
